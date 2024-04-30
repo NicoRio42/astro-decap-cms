b9bf690af36dc5ef74ad2f4a88290e25d4006d3e
Nicolas Rio <nicolas.rio42@gmail.com>
2024-04-30 15:17:24 +0200

    Better redirect url

--- src/oauth/index.ts
+++ src/oauth/index.ts
@@ -2,10 +2,10 @@ import type { APIRoute } from "astro";
 
 export const prerender = false;
 
-export const GET: APIRoute = ({ redirect, locals, url }) => {
+export const GET: APIRoute = ({ redirect, locals, site }) => {
   const { env } = locals.runtime;
 
-  const redirectUri = encodeURI(`${url.toString()}/callback`);
+  const redirectUri = encodeURI(`${site?.toString()}oauth/callback`);
 
   const authUrl = `https://github.com/login/oauth/authorize?client_id=${env.GITHUB_CLIENT_ID}&scope=repo,user&redirect_uri=${redirectUri}`;
 
