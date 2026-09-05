a9cbb0fe9831b3e654db5c149803752b40be5389
Nicolas Rio <nicolas.rio42@gmail.com>
2026-09-05 12:49:17 +0200

    Fix vit vs CF issue

--- src/index.js
+++ src/index.js
@@ -28,6 +28,24 @@ function virtualModules(modules) {
 }
 
 /**
+ * @param {string[]} dependencies
+ */
+function optimizeServerDependencies(dependencies) {
+  return {
+    name: "astro-decap-cms:optimize-server-dependencies",
+    configEnvironment(/** @type {string} */ name) {
+      if (name !== "ssr") return;
+
+      return {
+        optimizeDeps: {
+          include: dependencies,
+        },
+      };
+    },
+  };
+}
+
+/**
  * @param {import("./types.js").DecapCmsIntegrationOptions} astroDecapConfig
  * @returns {import("astro").AstroIntegration}
  */
@@ -66,6 +84,14 @@ export default function decapCMS(astroDecapConfig) {
                       "virtual:astro-decap-cms": virtualModule,
                     }
               ),
+              ...(injectOAuthRoute
+                ? [
+                    optimizeServerDependencies([
+                      "astro-decap/src/oauth/index.ts",
+                      "astro-decap/src/oauth/callback.ts",
+                    ]),
+                  ]
+                : []),
             ],
           },
         });
